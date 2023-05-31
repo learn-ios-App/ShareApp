68cd1f71b3f964f5afbea3506bfae27130893bd1
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-05-31 16:16:21 +0900

    Initial Commit

---
+++ ShareApp/ShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareAppApp.swift
+//  ShareApp
+//
+//  Created by 渡邊魁優 on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct ShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
